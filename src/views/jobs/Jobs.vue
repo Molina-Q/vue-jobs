466e5bfa6040410b856f9a6e6c83ef77650240fc
Molina-Q <quentinmolina1@gmail.com>
2024-07-04 14:30:19 +0200

    feat: Add Jobs view with list of job titles

---
+++ src/views/jobs/Jobs.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Jobs</h1>
+    <div v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            jobs: [
+                { title: 'Frontend Developer', id: 1, details: 'Frontend Developer details' },
+                { title: 'Backend Developer', id: 2, details: 'Backend Developer details' },
+                { title: 'Fullstack Developer', id: 3, details: 'Fullstack Developer details'}
+            ]
+        }
+    }
+}
+</script>
+
+<style></style>
